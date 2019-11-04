a8bd1aac45343017f7826d5d5b80c0fd03a92d50
Néfix Estrada <nefixestrada@gmail.com>
2019-11-04 10:28:04 +0000

    Added simple documentation for the transport and transport/grpc packages

---
+++ transport/grpc/doc.go
@@ -0,0 +1,4 @@
+// Package grpc is the responsible for implementing
+// the GRPC service defined at github.com/brainupdaters/drlm-common/pkg/proto
+// It basically parses the requests and calls the other packages
+package grpc
