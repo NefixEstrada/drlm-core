e0b9fea9014f25511014c5d8fc1459b28c20e1fa
Néfix Estrada <nefixestrada@gmail.com>
2020-03-19 09:22:07 +0000

    Rewritten the migrations correctly
    As advised in https://github.com/go-gormigrate/gormigrate#usage,
    it's a good practice to insert the whole type in each migration, because
    if they are modified, they could break the whole migration system

--- db/migrations/migrations.go
+++ db/migrations/migrations.go
@@ -3,8 +3,10 @@
 package migrations
 
 import (
+	"sync"
+	"time"
+
 	"github.com/brainupdaters/drlm-core/context"
-	"github.com/brainupdaters/drlm-core/models"
 
 	"github.com/jinzhu/gorm"
 	log "github.com/sirupsen/logrus"
@@ -17,7 +19,16 @@ func Migrate(ctx *context.Context) {
 		{
 			ID: "201907161002",
 			Migrate: func(tx *gorm.DB) error {
-				return tx.AutoMigrate(&models.User{}).Error
+				type Type int
+
+				type User struct {
+					gorm.Model
+					Username string `gorm:"unique;not null"`
+					Password string `gorm:"not null"`
+					AuthType Type   `gorm:"not null"`
+				}
+
+				return tx.AutoMigrate(&User{}).Error
 			},
 			Rollback: func(tx *gorm.DB) error {
 				return tx.DropTable("users").Error
@@ -26,7 +37,30 @@ func Migrate(ctx *context.Context) {
 		{
 			ID: "201910161152",
 			Migrate: func(tx *gorm.DB) error {
-				return tx.AutoMigrate(&models.Agent{}).Error
+				type Arch int
+				type OS int
+
+				type Agent struct {
+					gorm.Model
+					Host     string `gorm:"unique;not null"`
+					Accepted bool   `gorm:"not null"`
+
+					MinioKey string `gorm:"not null"`
+					Secret   string `gorm:"unique;not null"` // The secret is used for authentication
+
+					SSHPort     int `gorm:"not null"`
+					SSHUser     string
+					SSHHostKeys string `gorm:"size:9999"` // The different keys are splitted with `|||` between each one
+
+					Version       string
+					Arch          Arch
+					OS            OS
+					OSVersion     string
+					Distro        string
+					DistroVersion string
+				}
+
+				return tx.AutoMigrate(&Agent{}).Error
 			},
 			Rollback: func(tx *gorm.DB) error {
 				return tx.DropTable("agents").Error
@@ -35,7 +69,24 @@ func Migrate(ctx *context.Context) {
 		{
 			ID: "201910161153",
 			Migrate: func(tx *gorm.DB) error {
-				return tx.AutoMigrate(&models.Job{}).Error
+				type JobStatus int
+
+				type Job struct {
+					gorm.Model
+
+					PluginID   uint      `gorm:"not null"`
+					AgentHost  string    `gorm:"not null"`
+					Status     JobStatus `gorm:"not null"`
+					Time       time.Time
+					Config     string `gorm:"not null"`
+					BucketName string `gorm:"not null;unique"`
+					Info       string
+
+					Mux            sync.Mutex `gorm:"-"`
+					ReconnAttempts int
+				}
+
+				return tx.AutoMigrate(&Job{}).Error
 			},
 			Rollback: func(tx *gorm.DB) error {
 				return tx.DropTable("jobs").Error
@@ -44,7 +95,42 @@ func Migrate(ctx *context.Context) {
 		{
 			ID: "202001271226",
 			Migrate: func(tx *gorm.DB) error {
-				return tx.AutoMigrate(&models.Plugin{}).Error
+				type Arch int
+				type OS int
+
+				type Agent struct {
+					gorm.Model
+					Host     string `gorm:"unique;not null"`
+					Accepted bool   `gorm:"not null"`
+
+					MinioKey string `gorm:"not null"`
+					Secret   string `gorm:"unique;not null"` // The secret is used for authentication
+
+					SSHPort     int `gorm:"not null"`
+					SSHUser     string
+					SSHHostKeys string `gorm:"size:9999"` // The different keys are splitted with `|||` between each one
+
+					Version       string
+					Arch          Arch
+					OS            OS
+					OSVersion     string
+					Distro        string
+					DistroVersion string
+				}
+
+				type Plugin struct {
+					gorm.Model
+					Repo      string `gorm:"not null"`
+					Name      string `gorm:"not null"`
+					Version   string `gorm:"not null"`
+					AgentHost string `gorm:"not null"`
+					Agent     *Agent `gorm:"foreignkey:Host;association_foreignkey:AgentHost"`
+					// TODO: This should be stored in the DB
+					Arch []Arch `gorm:"-"`
+					OS   []OS   `gorm:"-"`
+				}
+
+				return tx.AutoMigrate(&Plugin{}).Error
 			},
 			Rollback: func(tx *gorm.DB) error {
 				return tx.DropTable("plugins").Error
